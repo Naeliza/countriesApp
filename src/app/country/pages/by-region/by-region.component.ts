29730ae21191e96d4708383968b18a32048eaff5
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-07-03 21:47:19 -0400

    ngClass, class y [class]

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -3,14 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
-  styles: [
+  styles: [`
+  button {
+    margin-right: 5px;
+  }`
   ]
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
 
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
+  
   constructor() { }
 
-  ngOnInit(): void {
+  activateRegion(region: string)
+  {
+    this.activeRegion = region;
+
+    //TODO: HACER EL LLAMADO AL SERVICIO
   }
 
 }
