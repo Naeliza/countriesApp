a68f43bbdde542cb634ae7eef5257f2aabc79517
Naomi Cespedes <lizcespedes0326@gmail.com>
2021-07-03 20:24:30 -0400

    Finishing main content of Country Information page

--- src/app/country/pages/look-country/look-country.component.ts
+++ src/app/country/pages/look-country/look-country.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/country.interface';
 import { CountryService } from '../../services/country.service';
 
 @Component({
@@ -11,15 +12,16 @@ import { CountryService } from '../../services/country.service';
 })
 export class LookCountryComponent implements OnInit {
 
+  country!: Country;
+
   constructor(private activatedRoute: ActivatedRoute, private _countryService: CountryService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap(({ id }) => this._countryService.getCountryByCode(id)))
-      .subscribe(resp => {
-        console.log(resp)
-      });
+        switchMap(({ id }) => this._countryService.getCountryByCode(id)),
+        tap(console.log)) //Imprime lo que recibe al hacer el switchMap porque tap se mantiene oyendo los cambios
+      .subscribe(country => this.country = country );
   }
 
 }
